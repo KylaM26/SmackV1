74668f460bf5e54685023fb49bc380fa03254b70
Kyla <iheartkyla@yahoo.com>
2018-06-21 09:06:35 -0500

    CustomizationSWReveal

---
+++ SmackV1/Controller/ChannelVC.swift
@@ -0,0 +1,18 @@
+//
+//  ChannelVC.swift
+//  SmackV1
+//
+//  Created by Kyla Wilson on 6/21/18.
+//  Copyright © 2018 Kyla. All rights reserved.
+//
+
+import UIKit
+
+class ChannelVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60;
+    }
+
+}
---
+++ SmackV1/Controller/ChatVC.swift
@@ -0,0 +1,23 @@
+//
+//  ChatVC.swift
+//  SmackV1
+//
+//  Created by Kyla Wilson on 6/21/18.
+//  Copyright © 2018 Kyla. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+    
+    @IBOutlet weak var revealBtn: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        revealBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: UIControlEvents.touchUpInside);
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer());
+        self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer());
+    }
+
+}
